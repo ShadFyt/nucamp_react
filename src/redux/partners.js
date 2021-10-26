11a4efef214b4259cc267f80b1450a7cde7d65f5
ShadFyt <doanryan10@gmail.com>
2021-10-25 22:30:47 -0500

    reducers

---
+++ src/redux/partners.js
@@ -0,0 +1,9 @@
+import { COMMENTS } from "../shared/comments";
+import { PARTNERS } from "../shared/partners";
+
+export const Partners = (state = COMMENTS, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
